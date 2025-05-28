8aeb94821a7a237fcd5198dcc9d3293dfe99568f
sahrajnish <raj.rajnish.sah@gmail.com>
2025-05-28 21:05:50 +0530

    Add contact us section

--- myapp06/src/components/Contact/Contact.jsx
+++ myapp06/src/components/Contact/Contact.jsx
@@ -61,7 +61,7 @@ const Contact = () => {
       errors.message = "Message is required";
     }
 
-    if(Object.keys(errors).length) {
+    if (Object.keys(errors).length) {
       setFormErrors(errors);
       setIsLoading(false);
       return
@@ -74,6 +74,8 @@ const Contact = () => {
       console.log(res);
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -186,10 +188,33 @@ const Contact = () => {
 
               {/* Submit Button with Blue Color & Shadow */}
               <button
+                disabled={isLoading}
                 type="submit"
                 className="w-full bg-blue-600 text-white font-bold py-3 px-6 rounded-lg shadow-xl hover:bg-blue-500 transition duration-300"
               >
                 Submit
+                {isLoading && (
+                  <svg
+                    className="animate-spin ml-2 h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="currentColor"
+                      strokeWidth="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="currentColor"
+                      d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+                    ></path>
+                  </svg>
+                )}
               </button>
             </form>
           </div>
